e43813c2d716d84924a379d618e3a4791df4679c
Waldo Lemmer <>
2023-11-07 13:23:36 +0200

    update comments

--- usr/local/bin/on-tablet-add-aux.sh
+++ usr/local/bin/on-tablet-add-aux.sh
@@ -33,9 +33,46 @@ set_device_properties() {
 configure_devices() {
 	for device_name in "$@"; do
 		wait_for_device "$device_name"
-		# TODO (area according to screen size)
+
+		# TODO (Area)
+		#
+		# 	Area <x1> <y1> <x2> <y2>
+		#
+		# Maps the screen area as follows:
+		# (x1, y1) ----------------------|
+		#    |                           |
+		#    |        screen area        |
+		#    |                           |
+		#    | (0, 0) ----------|        |
+		#    |   | drawing area |        |
+		#    |   |------- (32767, 32767) |
+		#    |                           |
+		#    |----------------------- (x2, y2)
+		#
+		# The following variables must all be measured in the same unit
+		# of length such as pixels or millimeters:
+		#
+		#        0  left         right  width
+		#         |--|-------------|------|--->
+		#         |                       |
+		#         |    physical screen    |
+		#         |                       |
+		#    top -| |--------------|      |
+		#         | | drawing area |      |
+		# bottom -| |--------------|      |
+		#         |                       |
+		# height -|-----------------------|
+		#         v
+		#
+		# These variables can then be used to calculate x1, y1, x2 and
+		# y2:
+		#
+		# x1 = -left / (right - left) * 32767
+		# y1 = -top / (bottom - top) * 32767
+		# x2 = (width - left) / (right - left) * 32767
+		# y2 = (height - top) / (bottom - top) * 32767
 		set_device_properties "$device_name" \
-			'Area -27268 -22937 59990 55704' \
+			'Area -28165 -10037 60932 70848' \
 			'Suppress 0' \
 			'RawSample 1' \
 			'Threshold 1'
